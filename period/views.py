21e7ec752fff3b5e0da42d6e2fc346d82d167fc2
Lordofmachine <akinwumikaliyanu@gmail.com>
2021-02-04 14:17:39 +0100

    updates

--- period/views.py
+++ period/views.py
@@ -6,8 +6,8 @@ def mainpage(request):
   submitbutton = request.POST.get("submit")
   ovulation_cycle=""
   first_day_of_last=""
-  context=""
   men_date=""
+  
   if request.method=='POST':
     form=PeriodForm(request.POST)
     if form.is_valid():
@@ -16,7 +16,7 @@ def mainpage(request):
       old_date=datetime.strptime(first_day_of_last,'%d-%m-%Y')
       est_date=timedelta(days=ovulation_cycle)
       men_date=old_date + est_date
-      context={'form':form,'men_date':men_date,'submitbutton':submitbutton}
+    
     else:
       return redirect('period:main')
   else:   
