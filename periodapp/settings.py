eb00f5ddf13c2577674cd9be53929ced326d44dc
Iyanu Akinwumi <akinwumikaliyanu@gmail.com>
2021-05-27 23:52:30 +0100

    changed data structure

--- periodapp/settings.py
+++ periodapp/settings.py
@@ -133,16 +133,6 @@ STATICFILES_DIRS=(os.path.join(BASE_DIR,'assets')),
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
-#EMAIL_HOST='smtp.live.com'
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER='drlifewritinghub@gmail.com'
-#EMAIL_HOST_USER='omoakinwumi@outlook.com'
-EMAIL_HOST_PASSWORD='kalel2000'
-
-
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=False
 EMAIL_HOST_USER='drlifewritinghub@gmail.com'
